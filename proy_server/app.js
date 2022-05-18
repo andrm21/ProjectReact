861a0bb9175df8e41d826812094f5966a9bfd460
Andrea <andrea.munozr@autonoma.edu.co>
2022-05-18 09:19:07 -0500

    Serve Funcionando

--- proy_server/app.js
+++ proy_server/app.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
 const { API_VERSION } = require("./config");
 
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
+const userRoutes = require("./src/routes/user");
 
-app.post("/api/v1/users",(req,res) =>{
-    const { name , email,password } = req.body;
-    const user = new userModel ({name , email,password});
-    user.save ((err,user) =>{
-        if(err) {
-            res.status(500).send(err);
-        }else{
-            res.status(201).send(user);
-        }
-    });
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 /* Configuración de los header HTTP */
 module.exports = app;
--- proy_server/index.js
+++ proy_server/index.js
@@ -1,20 +1,21 @@
-const mongoose = require("mongoose");
+const moongose = require("mongoose");
+const express = require("express");
 const app = require("./app");
 const PORT_SERVER = process.env.PORT || 3977;
-const { API_VERSION, IP_SERVER, PORT_DB } =  require("./config");
+const { API_VERSION, IP_SERVER, PORT_DB } = require("./config");
 
-mongoose.connect(
-    `mongodb://${IP_SERVER}:${PORT_DB}/proyect_db`,{ useNewUrlParser: true, useUnifiedTopology: true }, (err, res) =>{
-        if(err){
-            throw err;
-        } else {
-            console.log("success connection to DB");
-            app.listen(PORT_SERVER, () =>{
-                console.log("##############################");
-                console.log("########## API REST ##########");
-                console.log("##############################");
-                console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`);
-            });
-        }
-    }
-);
+moongose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/proyect_db`, (err, res) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Success connection with mongo");
+    app.listen(PORT_SERVER, () => {
+      console.log("##############");
+      console.log("###API REST###");
+      console.log("##############");
+      console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`);
+    });
+  }
+});
+
+
